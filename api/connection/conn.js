2cae03305ef38f5cc0f17a1ede9f95871f4b1ad5
bakar mokhtar <walaalpakar08@gmail.com>
2023-12-06 21:45:57 +0300

    Encryp database assets to env file

--- api/connection/conn.js
+++ api/connection/conn.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const db = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
-    password: process.env.PASSWORD,
+    password: "",
     database: process.env.DATABASE_NAME
 });
 
